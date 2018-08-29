5cf18fa88690eb4c72aa301ea5f330f90680819b
zhihong.zhong <qingtian88@live.com>
2018-08-29 14:23:46 +0800

    "added staticPagesController and some static pages"

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -7,4 +7,18 @@ use Illuminate\Http\Request;
 class StaticPagesController extends Controller
 {
     //
+    public function home(){
+        return view('home');
+        //return "主页";
+    }
+
+    public function help(){
+        return view('help');
+        //return "帮助页";
+    }
+
+    public function about(){
+        return view('about');
+        //return "关于";
+    }
 }
