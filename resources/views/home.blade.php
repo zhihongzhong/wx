c7069e91485050c2a6130e00b32cee155332f171
zhihong.zhong <qingtian88@live.com>
2018-08-30 11:17:40 +0800

    added some style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.default')
 @section('title','home')
 @section('content')
-<h1>home</h1>
+<!-- <h1>home</h1> -->
+<div class="jumbotron">
+    <h1>Hello laravel</h1>
+    <p class="lead">你现在看到的是<a href="#">Laravel 入门教程</a></p>
+    <p>一切，从这里开始。</p>
+    <p>
+        <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+</div>
 @stop
 
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,8 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <!-- <link rel="stylesheet" href="/css/common.css"> -->
 </head>
 <body>
+<header class="navbar navbar-fixed-top navbar-inverse">
+    <div class="nav-container">
+        <div class="col-md-offset-1 col-md-10">
+            <a href="/" id="logo">Sample App</a>
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="/help">帮助</a></li>
+                    <li><a href="#">登陆</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+<div class="container">
     @yield('content')
+</div>
+    
 </body>
 </html>
