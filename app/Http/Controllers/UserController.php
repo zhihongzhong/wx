8aa405a27fed9d737ad73e48a8730fd91cd65615
zhihong.zhong <qingtian88@live.com>
2018-08-31 11:24:18 +0800

    modified UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -12,6 +13,7 @@ class UserController extends Controller
     }
 
     public function show(User $user){
-        return view('user.show',compact('user'));
+        return view('users.show',compact('user'));
     }
+
 }
